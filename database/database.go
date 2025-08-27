2ad60e62ebc5b65ad1bae45594187c7a66bffdaa
Ikhfan Yusuf <ikhfan.yusuf@gmail.com>
2025-08-27 11:18:35 +0700

    chore:add env db

--- database/database.go
+++ database/database.go
@@ -3,7 +3,9 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,9 +14,22 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 
-	var err error
+	err := godotenv.Load()
 
-	dsn := "iya:xyzNyn-4@tcp(127.0.0.1:3306)/my-fin?charset=utf8mb4&parseTime=True&loc=Local"
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// ambil dari env
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	// susun DSN (Data Source Name)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, host, port, name)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
